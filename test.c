e31b360a4301f814eb3ada3a9b26eda4575d07a2
Aman Patel <amanofficially02@gmail.com>
2026-03-02 15:48:26 +0530

    C practise

---
+++ test.c
@@ -0,0 +1,83 @@
+// #include<stdio.h>
+// void main(){
+//     int num;
+//     printf("Enter any number : ");
+//     scanf("%d",&num);
+    
+//     if(num%2==0){
+//         printf("Even");
+//     }
+//     else{
+//         printf("Odd");
+
+//     }
+// }
+
+// #include<stdio.h>
+// void main (){
+//     int num1,num2,num3;
+//     printf("Enter first number : ");
+//     scanf("%d",&num1);
+//     printf("Enter second number : ");
+//     scanf("%d",&num2);
+//     printf("Enter third number : ");
+//     scanf("%d",&num3);
+
+
+//     if(num1>num2 && num1>num3){
+//         printf("%d is largest",num1);
+//     }
+//     else if (num2>num1 && num2>num3)
+//     {
+//         printf("%d is largest",num2);
+        
+//     }
+//     else{
+//         printf("%d is largest",num3);
+
+//     }
+// }
+
+// #include<stdio.h>
+// void main(){
+//     char c;
+//     printf("Enter any charcter : ");
+//     scanf("%c",&c);
+//     if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+//         printf("Vowel");
+//     }
+//     else{
+//         printf("consonant");
+//     }
+
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//     int marks;
+
+//     printf("Enter your marks: ");
+//     scanf("%d", &marks);
+
+//     if(marks >= 90 && marks <= 100) {
+//         printf("Grade A\n");
+//     }
+//     else if(marks >= 75) {
+//         printf("Grade B\n");
+//     }
+//     else if(marks >= 60) {
+//         printf("Grade C\n");
+//     }
+//     else if(marks >= 40) {
+//         printf("Grade D\n");
+//     }
+//     else if(marks >= 0) {
+//         printf("Fail\n");
+//     }
+//     else {
+//         printf("Invalid Marks\n");
+//     }
+
+//     return 0;
+// }
