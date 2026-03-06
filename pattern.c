c0ab83621852e1c44c7fb4ab969406a222ca7cde
Aman Patel <amanofficially02@gmail.com>
2026-03-06 17:24:46 +0530

    final commit

--- pattern.c
+++ pattern.c
@@ -346,23 +346,38 @@
 // }
 
 
+// #include<stdio.h>
+// int main(){
+//     int i,j;
+//     for(i=1;i<=4;i++){
+        
+//         // spaces print
+//         for(j=1;j<=4-i;j++){
+//             printf(" ");
+//         }
+
+//         // stars print
+//         for(j=1;j<=i;j++){
+//             printf("* ");
+//         }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int i,j;
-    for(i=1;i<=4;i++){
-        
-        // spaces print
-        for(j=1;j<=4-i;j++){
+    int i,j,k;
+    for(i=1; i<=5; i++){
+        for(j=1; j<i ;j++){
             printf(" ");
         }
-
-        // stars print
-        for(j=1;j<=i;j++){
-            printf("* ");
+        for(k=1; k<=6-i; k++){
+            printf("*");
         }
-
         printf("\n");
     }
-
-    return 0;
+    return 0 ;
 }
