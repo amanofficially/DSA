319731ccb2a171da38bb50f50796c1d34bf19ef5
Aman Patel <amanofficially02@gmail.com>
2026-03-11 12:56:39 +0530

    loops practise

--- pattern.c
+++ pattern.c
@@ -367,21 +367,44 @@
 //     return 0;
 // }
 
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+
+//     for(i=1;i<=5;i++){
+
+//         for(j=1;j<=5-i;j++){
+//             printf(" ");
+//         }
+
+//         for(k=1; k<=i; k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main(){
-    int i,j,k;
-
-    for(i=1;i<=5;i++){
-
-        for(j=1;j<=5-i;j++){
-            printf(" ");
+    int i,j;
+    for(i=1; i<=7; i++){
+        int limit;
+        if(i<=4){
+            limit=i;
         }
+        else{
+            limit=8-i;
+        }
+        for(j=1;j<=limit; j++){
 
-        for(k=1; k<=i; k++){
-            printf("*");
+            printf("* ");
         }
         printf("\n");
     }
 
     return 0;
 }
+
