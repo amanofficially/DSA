a05f71d762c9c65a04047163dfe4228bf0406e4d
Aman Patel <amanofficially02@gmail.com>
2026-03-14 20:40:00 +0530

    soting in array

--- sorting.c
+++ sorting.c
@@ -1,30 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void bubbleSort(int arr[], int n){
-    int i, j, temp;
+void bubbleSort(int arr[], int n)
+{
+    int i, j, swap;
 
-    for(i = 0; i < n-1; i++){
-        for(j = 0; j < n-i-1; j++){
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
 
-            if(arr[j] > arr[j+1]){
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+            if (arr[j] > arr[j + 1])
+            {
+                swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
             }
-
         }
     }
 }
 
-void printArray(int arr[], int n){
+void printArray(int arr[], int n)
+{
     printf("Sorted array: ");
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
 }
 
-int main(){
+int main()
+{
 
     int n;
 
@@ -35,11 +41,11 @@ int main(){
 
     printf("Enter %d elements:\n", n);
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
     }
 
     bubbleSort(arr, n);
     printArray(arr, n);
-
 }
