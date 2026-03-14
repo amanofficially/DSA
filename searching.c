bf2b916f80a22f5bc215f49256470aec2beb2f69
Aman Patel <amanofficially02@gmail.com>
2026-03-14 19:51:36 +0530

    searching

--- searching.c
+++ searching.c
@@ -1,46 +1,107 @@
-#include<stdio.h>
+/*----------------------------- Linear Search --------------------------*/
 
-//function declaration
-void linearSearch(int arr[], int n, int target);
+// #include<stdio.h>
 
-//main function
-int main(){
-    int n;
+// //function declaration
+// void linearSearch(int arr[], int n, int target);
+
+// //main function
+// int main(){
+//     int n;
+//     int target;
+
+//     printf("Enter the size of array:\n");
+//     scanf("%d",&n);
+
+//     int arr[n];
+
+//     printf("Enter %d elements:\n", n);
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+
+//     printf("Enter the element you want to search:\n");
+//     scanf("%d",&target);
+
+//     linearSearch(arr,n,target);
+
+//     return 0;
+// }
+
+// //function definition
+// void linearSearch(int arr[],int n,int target){
+
+//     int found = 0;
+
+//     for(int i=0;i<n;i++){
+//         if(arr[i] == target){
+//             printf("Target found at index %d\n",i);
+//             found = 1;
+//             break;
+//         }
+//     }
+
+//     if(found == 0){
+//         printf("Element not present in the array\n");
+//     }
+// }
+
+#include <stdio.h>
+
+// function declaration
+void binarySearch(int arr[], int size, int target);
+
+// main function
+int main()
+{
+    int size;
     int target;
 
     printf("Enter the size of array:\n");
-    scanf("%d",&n);
+    scanf("%d", &size);
 
-    int arr[n];
+    int arr[size];
 
-    printf("Enter %d elements:\n", n);
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    printf("Enter %d sorted elements:\n", size);
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
     }
 
     printf("Enter the element you want to search:\n");
-    scanf("%d",&target);
+    scanf("%d", &target);
 
-    linearSearch(arr,n,target);
+    binarySearch(arr, size, target);
 
     return 0;
 }
 
+// function definition
+void binarySearch(int arr[], int size, int target)
+{
+    int start = 0;
+    int end = size - 1;
 
-//function definition
-void linearSearch(int arr[],int n,int target){
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
 
-    int found = 0;
+        if (arr[mid] == target)
+        {
+            printf("Target found at index %d\n", mid);
+            return;
+        }
 
-    for(int i=0;i<n;i++){
-        if(arr[i] == target){
-            printf("Target found at index %d\n",i);
-            found = 1;
-            break;
+        else if (target > arr[mid])
+        {
+            start = mid + 1;
+        }
+
+        else
+        {
+            end = mid - 1;
         }
     }
 
-    if(found == 0){
-        printf("Element not present in the array\n");
-    }
+    printf("Element not found\n");
 }
