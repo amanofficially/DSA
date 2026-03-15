fe668f9b2797d25a0d9f24415c8c881bed69d68f
Aman Patel <amanofficially02@gmail.com>
2026-03-15 20:17:13 +0530

    sorting

--- sorting.c
+++ sorting.c
@@ -1,51 +1,98 @@
 #include <stdio.h>
 
+// Bubble Sort
 void bubbleSort(int arr[], int n)
 {
-    int i, j, swap;
+    int i, j, temp;
 
     for (i = 0; i < n - 1; i++)
     {
         for (j = 0; j < n - i - 1; j++)
         {
-
             if (arr[j] > arr[j + 1])
             {
-                swap = arr[j];
+                temp = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j + 1] = swap;
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-void printArray(int arr[], int n)
+// Selection Sort
+void selectionSort(int arr[], int n)
 {
-    printf("Sorted array: ");
+    int i, j, minIndex, temp;
 
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n - 1; i++)
     {
-        printf("%d ", arr[i]);
+        minIndex = i;
+
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
 
+// Insertion Sort
+void insertionSort(int arr[], int n)
+{
+    int i, j, key;
+
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+// Print Array
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Main Function
 int main()
 {
+    int arr1[] = {5, 3, 8, 1, 2};
+    int arr2[] = {5, 3, 8, 1, 2};
+    int arr3[] = {5, 3, 8, 1, 2};
 
-    int n;
+    int n = 5;
 
-    printf("Enter the size of Array: ");
-    scanf("%d", &n);
+    bubbleSort(arr1, n);
+    printf("Bubble Sort: ");
+    printArray(arr1, n);
 
-    int arr[n];
+    selectionSort(arr2, n);
+    printf("Selection Sort: ");
+    printArray(arr2, n);
 
-    printf("Enter %d elements:\n", n);
+    insertionSort(arr3, n);
+    printf("Insertion Sort: ");
+    printArray(arr3, n);
 
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    bubbleSort(arr, n);
-    printArray(arr, n);
+    return 0;
 }
