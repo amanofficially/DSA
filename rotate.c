ce364e258d40e9d48c059b8892f7c6d51ac056c7
Aman Patel <amanofficially02@gmail.com>
2026-03-17 15:47:30 +0530

    rotate

--- rotate.c
+++ rotate.c
@@ -1,17 +1,46 @@
+// #include <stdio.h>
+// void main()
+// {
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     int temp = arr[0];
+//     int n = 5;
+//     for (int i = 0; i < n; i++)
+//     {
+//         arr[i] = arr[i + 1];
+//     }
+//     arr[n - 1] = temp;
+//     printf("Rotated Array \n");
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+// }
 #include <stdio.h>
-void main()
+
+int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int temp = arr[0];
+    int arr[] = {1, 2, 3, 4, 5};
     int n = 5;
-    for (int i = 0; i < n; i++)
+    int k = 2;
+
+    for (int i = 0; i < k; i++)
     {
-        arr[i] = arr[i + 1];
+        int last = arr[n - 1]; // store last element
+
+        // shift all elements right
+        for (int j = n - 1; j > 0; j--)
+        {
+            arr[j] = arr[j - 1];
+        }
+
+        arr[0] = last; // put last at first
     }
-    arr[n - 1] = temp;
-    printf("Rotated Array \n");
+
+    // print array
     for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
+
+    return 0;
 }
