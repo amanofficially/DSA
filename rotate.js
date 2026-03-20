601cd58a7ee3f1cbe1872f6fd48f1c29a37ab675
Aman Patel <amanofficially02@gmail.com>
2026-03-20 14:09:48 +0530

    update

--- rotate.js
+++ rotate.js
@@ -12,20 +12,20 @@
 //   arr[arr.length - 1] = first;
 // }
 
-// console.log(arr);
+console.log(arr);
 
-// let arr = [10, 20, 30, 40, 50];
-// let k = 2; // number of rotations
+let arr = [10, 20, 30, 40, 50];
+let k = 2; // number of rotations
 
-// for (let i = 0; i < k; i++) {
-//   let last = arr[arr.length - 1];
+for (let i = 0; i < k; i++) {
+  let last = arr[arr.length - 1];
 
-//   // inner loop for shifting right
-//   for (let j = arr.length - 1; j > 0; j--) {
-//     arr[j] = arr[j - 1];
-//   }
+  // inner loop for shifting right
+  for (let j = arr.length - 1; j > 0; j--) {
+    arr[j] = arr[j - 1];
+  }
 
-//   arr[0] = last;
-// }
+  arr[0] = last;
+}
 
-// console.log(arr);
+console.log(arr);
