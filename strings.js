e7e989162eb2db01b7afdae055b41e3ccd27495a
Aman Patel <amanofficially02@gmail.com>
2026-03-23 15:22:43 +0530

    final commit

--- strings.js
+++ strings.js
@@ -7,20 +7,30 @@
 
 // console.log(newStr);
 
-function reverseArray(arr) {
-  let start = 0;
-  let end = arr.length - 1;
+// function reverseArray(arr) {
+//   let start = 0;
+//   let end = arr.length - 1;
 
-  while (start < end) {
-    let temp = arr[0];
-    arr[start] = arr[end];
-    arr[end] = temp;
+//   while (start < end) {
+//     let temp = arr[0];
+//     arr[start] = arr[end];
+//     arr[end] = temp;
 
-    start++;
-    end--;
-  }
+//     start++;
+//     end--;
+//   }
 
-  return arr;
+//   return arr;
+// }
+
+// console.log(reverseArray([1, 2, 3, 4, 5]));
+
+let str = "Aman";
+
+let newStr = "";
+
+for (let i = 0; i < str.length; i++) {
+  newStr += str[i];
 }
 
-console.log(reverseArray([1, 2, 3, 4, 5]));
+console.log(newStr);
