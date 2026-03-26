49ac1bd43211173b241355f5bd4101274a7a6dbc
Aman Patel <amanofficially02@gmail.com>
2026-03-26 16:19:31 +0530

    2D array

--- twoDarray.c
+++ twoDarray.c
@@ -197,3 +197,76 @@
 // }
 
 // ----------------------------- 1 --------------------------------
+
+// #include <stdio.h>
+// void main()
+// {
+//     int arr[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+//     int sum = 0;
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             if (i + j == 3)
+//             {
+//                 sum += arr[i][j];
+//             }
+//         }
+//     }
+//     printf("Sum of Primary Diagonal :- %d", sum);
+// }
+
+// ----------------------------- 2 --------------------------------
+
+// #include <stdio.h>
+// void main()
+// {
+//     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//     int sum = 0;
+//     printf("Transpose of Matrix -\n");
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 3; j++)
+//         {
+//             printf("%d ", arr[j][i]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// ----------------------------- 3 --------------------------------
+
+// #include <stdio.h>
+// void main()
+// {
+//     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//     int sum = 0;
+//     printf("Reverse of each row of an Matrix -\n");
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 2; j >= 0; j--)
+//         {
+//             printf("%d ", arr[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// ----------------------------- 4 --------------------------------
+
+#include <stdio.h>
+void main()
+{
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int sum = 0;
+    printf("Reverse of each row of an Matrix -\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 2; j >= 0; j--)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
