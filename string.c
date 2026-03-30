0e38273b8f716171d9c19c13c9b42d88f9fd42c2
Aman Patel <amanofficially02@gmail.com>
2026-03-30 18:02:48 +0530

    stirng final

--- string.c
+++ string.c
@@ -1,8 +1,40 @@
+// #include <stdio.h>
+// #include <string.h>
+// void main()
+// {
+//     char str[] = "Aman";
+//     int i = 0;
+//     for (i; str[i] != '\0'; i++)
+//     {
+//         printf("%c\n", str[i]);
+//     }
+// }
+
 #include <stdio.h>
-#include <string.h>
-void main()
+
+void reverse(char str[])
 {
-    char str[] = "Aman";
-    char str2[] = " Patel";
-    printf("%s", strcat(str, str2));
+    int i, length = 0;
+    char temp;
+
+    while (str[length] != '\0')
+    {
+        length++;
+    }
+
+    for (i = 0; i < length / 2; i++)
+    {
+        temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+
+    printf("Reversed string: %s", str);
+}
+
+int main()
+{
+    char str[] = "Programming";
+    reverse(str);
+    return 0;
 }
